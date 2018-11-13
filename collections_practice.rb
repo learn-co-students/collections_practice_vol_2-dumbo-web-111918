d62119e34447acbf0d941ef98bf90d133931e792
luijocroussett <ljcroussett@gmail.com>
2018-11-13 04:08:45 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,17 @@
 # your code goes here
+require "pry"
+
+def begins_with_r(array)
+  words_with_r = 0
+  binding.pry
+  all_begins_with_r = true
+  binding.pry
+  array.each do |word|
+    words_with_r += 1 if word.split()[0] == "r" || word.split()[0] == "R"
+    binding.pry
+  end
+  all_begins_with_r = words_with_r == array.size ? true : false
+  binding.pry
+  return all_begins_with_r
+  binding.pry
+end
