f50e50fe0a912eefd50212067c757ffbf2b1d9f5
Jonathan Biro <jonnybrx@gmail.com>
2018-11-14 04:53:48 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
-# your code goes here
+require "pry"
+def begins_with_r (tools)
+  tools.each do |tool|
+    if tool[0] != "r"
+      return false
+    end
+  end
+  true
+end
+
+=begin
+def begins_with_r (tools)
+  tools.all?{|tool| tool[0] == "r"}
+end
+=end
+
+def contain_a (arr)
+  arr.select do |element|
+    element.include? "a"
+  end
+end
+
+def first_wa (arr)
+  arr.find do |value|
+    value.to_s.start_with?("wa")
+  end
+end
+
+def remove_non_strings (arr)
+  arr.select do |element|
+    element.is_a? String
+  end
+end
+
+def count_elements (arr)
+
+end
